24a03318b25239e71d407e3e9418306af05208ad
Joe Dodd <joehdodd@gmail.com>
2024-05-28 15:34:32 -0400

    Mobile enhancements.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,29 @@ import { IconMenu2 } from "@tabler/icons-react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize(); // Call the function initially to set the state based on initial window size
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <header className="p-2 md:p-4 flex flex-row gap-4 justify-between items-start shadow-md">
-      <div>
+    <header className="p-2 md:p-4 flex flex-col md:flex-row gap-4 justify-between shadow-md">
+      <div className="flex flex-row gap-4 justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl text-center md:text-left md:text-2xl">
             Joe & Selah{" "}
@@ -21,14 +36,25 @@ const Header = () => {
           </h1>
           <hr className="hidden md:block mt-[-6px]" />
         </Link>
+        {isMobile && (
+          <div className="flex items-center">
+            <button onClick={toggleMenu} className="md:hidden">
+              <IconMenu2 size={24} />
+            </button>
+          </div>
+        )}
       </div>
-      <div className="md:text-2xl">
-        <button onClick={toggleMenu} className="md:hidden">
-          <IconMenu2 size={24} />
-        </button>
-        <div className={`${isMenuOpen ? '' : 'hidden'} md:block`}>
-          <Link href="/about">Info</Link>
-        </div>
+      <div
+        className={`${
+          isMobile && !isMenuOpen ? "hidden" : ""
+        } flex flex-col md:flex-row md:gap-4`}
+      >
+        <Link className="text-2xl" href="/">
+          Home
+        </Link>
+        <Link className="text-2xl" href="/about">
+          Info
+        </Link>
       </div>
     </header>
   );
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,14 +1,16 @@
 const Hero = () => {
   return (
     <div>
-      <div className="bg-hero bg-center bg-cover bg-no-repeat h-[600px] w-full">
+      <div className="bg-hero bg-center bg-cover bg-no-repeat h-[400px] md:h-[600px] w-full">
         <div className="h-full w-full flex justify-center items-center text-center text-white">
           <div>
-            <h3 className="text-6xl">Joe &amp; Selah</h3>
-            <hr/>
-            <h1 className="text-4xl">Chattanooga Photographers</h1>
-            <h3 classname="text-4xl">
-              Remember the day. &nbsp; As it happened.
+            <div className="hidden md:block">
+              <h3 className="text-6xl">Joe &amp; Selah</h3>
+              <hr />
+            </div>
+            <h1 className="text-3xl">Chattanooga Documentary Photographers</h1>
+            <h3 className="text-2xl">
+              Remember the day. As it happened.
             </h3>
           </div>
         </div>
