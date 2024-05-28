47668ad0b1b7a70d02183376834182463960cb2b
Joe Dodd <joehdodd@gmail.com>
2024-05-28 15:07:41 -0400

    Adds burger menu for head nav.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,34 @@
+import React from "react";
 import Link from "next/link";
+import { IconMenu2 } from "@tabler/icons-react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="p-2 md:p-4 flex flex-col md:flex-row gap-4 justify-between shadow-md">
+    <header className="p-2 md:p-4 flex flex-row gap-4 justify-between items-start shadow-md">
       <div>
         <Link href="/">
-          <h1 className="text-xl md:text-2xl">
-            Joe & Selah | Chattanooga Documentary Photographers
+          <h1 className="text-4xl text-center md:text-left md:text-2xl">
+            Joe & Selah{" "}
+            <span className="hidden md:inline">
+              | Chattanooga Documentary Photographers
+            </span>
           </h1>
-          <hr className="mt-[-6px]"/>
+          <hr className="hidden md:block mt-[-6px]" />
         </Link>
       </div>
       <div className="md:text-2xl">
-        <Link href="/about">Info</Link>
+        <button onClick={toggleMenu} className="md:hidden">
+          <IconMenu2 size={24} />
+        </button>
+        <div className={`${isMenuOpen ? '' : 'hidden'} md:block`}>
+          <Link href="/about">Info</Link>
+        </div>
       </div>
     </header>
   );
