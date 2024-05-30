2fe739d9110e4b83bf08baec273717d7e81339a6
Joe Dodd <joehdodd@gmail.com>
2024-05-30 10:27:50 -0400

    Adds wedding page copy.

--- src/pages/weddings.tsx
+++ src/pages/weddings.tsx
@@ -20,6 +20,19 @@ const Weddings = () => {
       />
       <PageLayout>
         <div className="p-2 md:p-4">
+          <div className="mb-4 md:p-12">
+            <h1 className="mb-4 text-3xl md:text-4xl font-bold text-center">
+              Chattanooga Wedding Photographers
+            </h1>
+            <p className="md:p-8 text-2xl text-center">
+              We are documentary-style wedding photographers in Chattanooga, TN
+              offering engagement and wedding photography. Our approach is to
+              capture as many of the moments of your day as they happen, rather
+              than staging them. We believe that these photos are the ones that
+              are most authentic to you and your relationship. We would love to
+              be a part of your special day!
+            </p>
+          </div>
           <ImageGrid imageSources={imageSources} />
         </div>
       </PageLayout>
